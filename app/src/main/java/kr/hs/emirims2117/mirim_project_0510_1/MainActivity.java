698117ba0b5dccf4c235e51c6dd1674451618bfb
곽경희 <82927462+kwakkyunghee@users.noreply.github.com>
2022-05-10 19:30:47 +0900

    0510 입력받고 출력하기 완성

--- app/src/main/java/kr/hs/emirims2117/mirim_project_0510_1/MainActivity.java
+++ app/src/main/java/kr/hs/emirims2117/mirim_project_0510_1/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,7 +13,8 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edit;
+    TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,9 +23,9 @@ public class MainActivity extends AppCompatActivity {
         LinearLayout.LayoutParams params=new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
                                                                         ViewGroup.LayoutParams.MATCH_PARENT);
         setContentView(baseLayout, params);
-        EditText edit=new EditText(this);
+        edit=new EditText(this);
         Button btn=new Button(this);
-        TextView text=new TextView(this);
+        text=new TextView(this);
         edit.setTextSize(20);
         edit.setTextColor(Color.BLACK);
         btn.setBackgroundColor(Color.YELLOW);
@@ -35,6 +37,15 @@ public class MainActivity extends AppCompatActivity {
         baseLayout.addView(edit);
         baseLayout.addView(btn);
         baseLayout.addView(text);
+//입력받고 출력하기
+        btn.setOnClickListener(btnLIstener);
 
     }
+
+    View.OnClickListener btnLIstener=new View.OnClickListener(){
+        @Override
+        public void onClick(View view){
+            text.setText(edit.getText().toString());
+        }
+    };
 }
